0509224393b99bcf8aa51a6bebb59d89bd3aae9e
sanatashu <sachamclements364@gmail.com>
2026-02-10 10:16:39 -0800

    update with demo06 UI elements

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap';
 
 // If you have custom global styles, import them as well:
-import '/styles/style.css';
+import '../styles/style.css';
 
 function sayHello() {
   // TODO: implement your logic here
